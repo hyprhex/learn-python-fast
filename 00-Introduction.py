df437ad47055d2e9df47fcc51e2c3c0afb400c1e
Aipills <swapcode.work@gmail.com>
2023-07-09 07:38:01 +0100

    Intorduction about Python

---
+++ 00-Introduction.py
@@ -0,0 +1,22 @@
+"""
+Who create Python?
+---
+Developed by Guido van Rossum in late 1980 and released 1991. 
+
+Why do people choose Python as the first programming language to learn?
+---
+Because of 3 things
+1. Popular
+2. Simple
+3. Easy to learn
+
+How use Python?
+---
+Big companies like: Google, Youtube, Amazon, Instagram ...
+
+Which Python version we use?
+---
+we will use Python3
+
+Yeeeeeeeeeep Python it's open source project
+"""
